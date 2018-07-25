1f63fd1890d478c5fb9f2d2a5247a6cd7cd736e8
Ryerson Computer networks 2018 case study <40874691+project29@users.noreply.github.com>
2018-07-25 00:13:55 -0400

    Add files via upload

---
+++ 1-1telnet-show version.py
@@ -0,0 +1,32 @@
+import telnetlib
+import datetime
+import time
+import sys
+import getpass
+
+
+print("\n***************************")
+
+HOST = input("IP Address: \n")
+user = input("Enter remote account: \n")
+password = getpass.getpass()
+print ("You entered:", password)
+
+tn = telnetlib.Telnet(HOST)
+
+#tn.read_until(b"Username: ")
+tn.read_until(b"Username: ")
+print("Username username is: ",user)
+
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+a=tn.read_until(b"Username: ",timeout=0.5)
+if len(a)<=2:
+    print ("wrong user or password")
+else:
+	tn.write(b"terminal length 0\n")
+	tn.write(b"show version\n\n")	
+	tn.write(b"exit\n")
+	print(tn.read_all().decode('ascii'))
